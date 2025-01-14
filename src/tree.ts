987967e97d7472ecff627f78dc52c229c3d2d0f3
Artem Mikhaylov <simplerick@yandex.com>
2025-01-14 16:58:27 +0300

    tree node implementation

--- src/tree.ts
+++ src/tree.ts
@@ -11,27 +11,24 @@ export class Tree extends TreeDragAndDropController implements vscode.TreeDataPr
     nodes: TreeNode[] = [];
 
 	public exampleTree: any = [
-        {"data": "a", "children": [
-            {"data": "aa", "children": [
-                {"data": "aaa", "children": [
-                    {"data": "aaaa"},
-                ]},
-                {"data": "aab"},
-            ]},
-        ]},
-        {"data": "c"},
-        {"data": "b", "children": [
-            {"data": "ba"},
-            {"data": "bb"},
-        ]},
-    ];
+		{"type": "group", "name": "Group 1", "children": [
+			{"type": "project", "name": "Project 1", "absolutePath": "/Users/simple/Code/mac-setup"},
+			{"type": "group", "name": "Group 2", "children": [
+				{"type": "project", "name": "Project 2", "absolutePath": "/wrong-path"},
+			]},
+		]},
+		{"type": "group", "name": "Group 3", "children": [
+			{"type": "project", "name": "Project 1", "absolutePath": "/Users/simple/Code/mac-setup"},
+			{"type": "group", "name": "Group 2", "children": [
+				{"type": "project", "name": "Project 4", "absolutePath": "/wrong-path"},
+			]},
+		]}
+	];
 
 	constructor(private extensionContext: vscode.ExtensionContext) {
 		super();
-		this.root = this._buildNodes("root", this.exampleTree);
+		this.root = this._buildNodes({type: "root", children: this.exampleTree});
 		this.extensionContext = extensionContext;
-
-
 	}
 
 	getTreeItem(element: TreeNode): vscode.TreeItem {
@@ -49,10 +46,11 @@ export class Tree extends TreeDragAndDropController implements vscode.TreeDataPr
         return element.parent;
     }
 
-	_buildNodes(data: any, children: any): TreeNode {
-		let childNodes: TreeNode[] = children.map((child: any) => this._buildNodes(child.data, child.children || []));
+	_buildNodes(nodeItem: any): TreeNode {
+		const {children = [], ...data} = nodeItem;
+		let childNodes: TreeNode[] = children.map((child: any) => this._buildNodes(child));
         let id = this.nodes.length;
-        let node = new TreeNode(id, data, undefined, childNodes);
+        let node = new TreeNode(id, undefined, childNodes, data);
 		childNodes.forEach(child => child.parent = node);
 		this.nodes.push(node);
         return node;
--- src/treeNode.ts
+++ src/treeNode.ts
@@ -5,75 +5,82 @@ import * as path from 'path';
 
 export class TreeNode {
 	id: number;
-	data: vscode.TreeItem;
 	parent?: TreeNode;
 	children: TreeNode[];
+	data: Group | Project;
 
-	constructor(id: number, data: any, parent?: TreeNode, children?: TreeNode[]) {
+	constructor(id: number, parent?: TreeNode, children?: TreeNode[], data?: any) {
 		this.id = id;
-		// this.data = data;
 		this.parent = parent;
 		this.children = children ?? [];
-		this.data = this.getTreeItem(data, id, this.children);
+		this.data = this.parseTreeItem(data);
 	}
 
-	getTreeItem(data: any, nodeId: number, children: any[]): vscode.TreeItem { 
-		return {
-			id: String(nodeId),
-			label: data,
-			collapsibleState: children.length ? vscode.TreeItemCollapsibleState.Collapsed : vscode.TreeItemCollapsibleState.None,
-		};
+	parseTreeItem(data: any): Group | Project {
+		// const stringId = String(id);
+		const {type, name, absolutePath, description, ..._} = data;
+
+		switch (type) { 
+			case "group":
+				return new Group(name, vscode.TreeItemCollapsibleState.Collapsed);
+			case "project":
+				return new Project(absolutePath, name, description);
+			case "root":
+				return new Group("root");
+			default:
+				throw new Error(`Unknown node type: ${type}`);
+		}
 	}
 }
 
 
-// export class Group extends vscode.TreeItem {
-// 	resourceUri = vscode.Uri.parse('_.js');
-// 	iconPath = path.join(__filename, '..', '..', 'assets', 'group_filled.svg');
+export class Group extends vscode.TreeItem {
+	resourceUri = vscode.Uri.parse('_.js');
+	iconPath = path.join(__filename, '..', '..', 'assets', 'group_filled.svg');
+	contextValue = 'group';
 
-// 	constructor(public readonly label: string,
-// 				public readonly collapsibleState?: vscode.TreeItemCollapsibleState,
-// 				// public readonly description?: string,
-// 	) {
-// 		super(label, collapsibleState);
-// 		// this.tooltip = description;
-// 		super(label, vscode.TreeItemCollapsibleState.Collapsed);
-// 	}
-// }
+	constructor(public readonly label: string,
+				public readonly collapsibleState?: vscode.TreeItemCollapsibleState,
+				// public readonly description?: string,
+	) {
+		super(label, collapsibleState);
+		// this.tooltip = description;
+	}
+}
 
 
-// export class Project extends vscode.TreeItem {
-// 	resourceUri = vscode.Uri.parse('_.js');
-//     iconPath = vscode.ThemeIcon.Folder;
-// 	valid = true;
+export class Project extends vscode.TreeItem {
+	resourceUri = vscode.Uri.parse('_.js');
+    iconPath = vscode.ThemeIcon.Folder;
+	valid = true;
+	contextValue = 'project';
 
-// 	constructor(
-//         public readonly absolutePath: string,
-// 		public readonly label?: string,
-// 		public readonly description?: string,
-// 	) {
-// 		// if label is not given take stem of absolutePath
-// 		label = label || absolutePath.split('/').pop()!;
-// 		super(label, vscode.TreeItemCollapsibleState.None);
-//         this.absolutePath = this.checkPath(absolutePath);
-// 		console.log(this.absolutePath);
-// 		this.tooltip = `${this.absolutePath}`;
-// 		this.description = this.description;
-// 	}
+	constructor(
+        public readonly absolutePath: string,
+		public readonly label?: string,
+		public readonly description?: string,
+	) {
+		// if label is not given take stem of absolutePath
+		label = label || absolutePath.split('/').pop()!;
+		super(label, vscode.TreeItemCollapsibleState.None);
+        this.absolutePath = this.checkPath(absolutePath);
+		this.tooltip = `${this.absolutePath}`;
+		this.description = this.description;
+	}
 
-//     // check if path exists
-// 	private checkPath(p: string): string {
-// 		try {
-// 			fs.accessSync(p);
-// 		} catch {
-// 			this.valid = false;
-// 			this.iconPath = new vscode.ThemeIcon('warning', new vscode.ThemeColor('editorWarning.foreground'));
-// 		}
-// 		return p;
-// 	}
+    // check if path exists
+	private checkPath(p: string): string {
+		try {
+			fs.accessSync(p);
+		} catch {
+			this.valid = false;
+			this.iconPath = new vscode.ThemeIcon('warning', new vscode.ThemeColor('editorWarning.foreground'));
+		}
+		return p;
+	}
 
-//     openFolder(newWindow: boolean = false) {
-//         const uri = vscode.Uri.file(this.absolutePath);
-//         vscode.commands.executeCommand('vscode.openFolder', uri, newWindow);
-//     }
-// }
+    openFolder(newWindow: boolean = false) {
+        const uri = vscode.Uri.file(this.absolutePath);
+        vscode.commands.executeCommand('vscode.openFolder', uri, newWindow);
+    }
+}
